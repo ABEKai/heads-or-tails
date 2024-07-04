4cbe8042c34e70fbd3df666e1eb76854053dc2ff
ABEKai <s1290107@u-aizu.ac.jp>
2024-07-04 17:32:46 +0900

    HoT: initial revision

---
+++ hot.py
@@ -0,0 +1,14 @@
+import random
+
+print("Tossing a coin...")
+heads = 0
+tails = 0
+for i in range(1, 4):
+    result = random.choice(['Heads', 'Tails'])
+    print(f"Round {i}: {result}")
+    if result == 'Heads':
+        heads += 1
+    else:
+        tails += 1
+
+print(f"Heads: {heads}, Tails: {tails}")
